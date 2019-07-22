ddb4556ab3527e6180d15115e14584964ede1eeb
adajett <adajett@gmail.com>
2019-07-22 15:43:59 -0700

    set-up

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render() {
+    return <form action="" className="store-selector"></form>
+	}
+}
+
+export default StorePicker;
